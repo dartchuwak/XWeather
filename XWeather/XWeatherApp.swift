85d8bd541ae61a68c94c5c5f6407c427c0ea42a3
Evgenii Mikhailov <e.mikhailov@outlook.com>
2025-12-24 18:46:02 +0300

    Initial Commit

---
+++ XWeather/XWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  XWeatherApp.swift
+//  XWeather
+//
+//  Created by Evgenii Mikhailov on 24.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct XWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
